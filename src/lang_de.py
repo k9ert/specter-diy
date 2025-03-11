04c531c157008084693ac5c97ae1eb231820721f
louisthecat86 <louiskatze1@web.de>
2025-03-11 19:17:46 +0100

    Update lang_de.py

--- src/lang_de.py
+++ src/lang_de.py
@@ -1,7 +1,12 @@
 translations = {
-  "Are you sure?": "Sind Sie sicher?",
-  "Error": "Fehler",
-  "Invalid word count": "Fehlerhafte Wortzahl",
-  "Invalid padding": "Fehlerhafte Eingabe",
-  
+  "Choose your PIN code": "PIN Code Eingabe",
+  "Remember these words," "they will stay the same on this device.": "Merke dir dieses Wort," "Nach der Pin Eingabe wird immer das gleiche Prüfwort angezeigt",
+  "Confirm your PIN code": "Bestätige deinen PIN Code",
+  "Error!", "PIN codes are different!": "Fehler!", "PIN Code ist nicht identisch!",
+  "First enter your old PIN code": "Bitte gib zuerst deinen alten PIN Code ein",
+  "Verifying PIN code...": "Bestätige PIN Code...",
+  "Setting new PIN code...": "Neuer PIN Code gesetzt...",
+  "Success!", "PIN code is successfully changed!": "Erfolgreich!", "PIN Code wurde erfolgreich geaendert!",
+  "Warning": "Warnung",
+  "You need to confirm your PIN code ": "Du musst deinen PIN Code bestaetigen",
   
