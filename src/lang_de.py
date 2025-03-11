2dcdda251457179b1b5d43374058452e04cb99a3
louisthecat86 <louiskatze1@web.de>
2025-03-11 12:38:01 +0100

    Update lang_de.py

--- src/lang_de.py
+++ src/lang_de.py
@@ -1,1 +1,7 @@
-
+translations = {
+  "Are you sure?": "Sind Sie sicher?",
+  "Error": "Fehler",
+  "Invalid word count": "Fehlerhafte Wortzahl",
+  "Invalid padding": "Fehlerhafte Eingabe",
+  
+  
